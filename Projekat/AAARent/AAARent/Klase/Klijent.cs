42c3d0969cf362f620209cbb96137dadbbd0439c
Ajdin <Ajdin@DESKTOP-7662KSG>
2018-04-30 14:16:28 -0700

    klase.

--- Projekat/AAARent/AAARent/Klase/Klijent.cs
+++ Projekat/AAARent/AAARent/Klase/Klijent.cs
@@ -4,9 +4,122 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace AAARent.Klase
 {
     class Klijent
     {
+        static int ID=1;
+        int id;
+        string ime;
+        string prezime;
+        string brojTelefona;
+        string eMail;
+        DateTime datumRodjenja;
+        DateTime datumRegistracije;
+
+
+        public Klijent(int id, string ime, string prezime, string brojTelefona, string eMail, DateTime datumRodjenja, DateTime datumRegistracije)
+        {
+            this.Id = id;
+            this.Ime = ime;
+            this.Prezime = prezime;
+            this.BrojTelefona = brojTelefona;
+            this.EMail = eMail;
+            this.DatumRodjenja = datumRodjenja;
+            this.DatumRegistracije = datumRegistracije;
+        }
+
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Ime
+        {
+            get
+            {
+                return ime;
+            }
+
+            set
+            {
+                ime = value;
+            }
+        }
+
+        public string Prezime
+        {
+            get
+            {
+                return prezime;
+            }
+
+            set
+            {
+                prezime = value;
+            }
+        }
+
+        public string BrojTelefona
+        {
+            get
+            {
+                return brojTelefona;
+            }
+
+            set
+            {
+                brojTelefona = value;
+            }
+        }
+
+        public string EMail
+        {
+            get
+            {
+                return eMail;
+            }
+
+            set
+            {
+                eMail = value;
+            }
+        }
+
+        public DateTime DatumRodjenja
+        {
+            get
+            {
+                return datumRodjenja;
+            }
+
+            set
+            {
+                datumRodjenja = value;
+            }
+        }
+
+        public DateTime DatumRegistracije
+        {
+            get
+            {
+                return datumRegistracije;
+            }
+
+            set
+            {
+                datumRegistracije = value;
+            }
+        }
     }
 }
--- Projekat/AAARent/AAARent/Klase/Vozilo.cs
+++ Projekat/AAARent/AAARent/Klase/Vozilo.cs
@@ -22,6 +22,208 @@ namespace AAARent.Klase
         double masa;
         double cijena;
         bool naStanju;
-        
+        // fali slika
+        double lokacijaX;
+        double lokacijaY;
+
+        public Vozilo(string registracija, string tip, string model, string proizvodjac, int godiste, string boja, string tipGoriva, int konjskihSnaga, double kubikaza, double masa, double cijena, bool naStanju, double lokacijaX, double lokacijaY)
+        {
+            this.registracija = registracija;
+            this.tip = tip;
+            this.model = model;
+            this.proizvodjac = proizvodjac;
+            this.godiste = godiste;
+            this.boja = boja;
+            this.tipGoriva = tipGoriva;
+            this.konjskihSnaga = konjskihSnaga;
+            this.kubikaza = kubikaza;
+            this.masa = masa;
+            this.cijena = cijena;
+            this.naStanju = naStanju;
+            this.lokacijaX = lokacijaX;
+            this.lokacijaY = lokacijaY;
+        }
+
+        public string Registracija
+        {
+            get
+            {
+                return registracija;
+            }
+
+            set
+            {
+                registracija = value;
+            }
+        }
+
+        public string Tip
+        {
+            get
+            {
+                return tip;
+            }
+
+            set
+            {
+                tip = value;
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+
+            set
+            {
+                model = value;
+            }
+        }
+
+        public string Proizvodjac
+        {
+            get
+            {
+                return proizvodjac;
+            }
+
+            set
+            {
+                proizvodjac = value;
+            }
+        }
+
+        public int Godiste
+        {
+            get
+            {
+                return godiste;
+            }
+
+            set
+            {
+                godiste = value;
+            }
+        }
+
+        public string Boja
+        {
+            get
+            {
+                return boja;
+            }
+
+            set
+            {
+                boja = value;
+            }
+        }
+
+        public string TipGoriva
+        {
+            get
+            {
+                return tipGoriva;
+            }
+
+            set
+            {
+                tipGoriva = value;
+            }
+        }
+
+        public int KonjskihSnaga
+        {
+            get
+            {
+                return konjskihSnaga;
+            }
+
+            set
+            {
+                konjskihSnaga = value;
+            }
+        }
+
+        public double Kubikaza
+        {
+            get
+            {
+                return kubikaza;
+            }
+
+            set
+            {
+                kubikaza = value;
+            }
+        }
+
+        public double Masa
+        {
+            get
+            {
+                return masa;
+            }
+
+            set
+            {
+                masa = value;
+            }
+        }
+
+        public double Cijena
+        {
+            get
+            {
+                return cijena;
+            }
+
+            set
+            {
+                cijena = value;
+            }
+        }
+
+        public bool NaStanju
+        {
+            get
+            {
+                return naStanju;
+            }
+
+            set
+            {
+                naStanju = value;
+            }
+        }
+
+        public double LokacijaX
+        {
+            get
+            {
+                return lokacijaX;
+            }
+
+            set
+            {
+                lokacijaX = value;
+            }
+        }
+
+        public double LokacijaY
+        {
+            get
+            {
+                return lokacijaY;
+            }
+
+            set
+            {
+                lokacijaY = value;
+            }
+        }
     }
 }
