429a5cb82023f43ab197c3efebdf3c0ecd406806
aldincelik <aldincelik@gmail.com>
2018-05-10 01:26:52 +0200

    Doradjena administratorska forma

--- Projekat/AAARent/AAARent/Administrator.xaml.cs
+++ Projekat/AAARent/AAARent/Administrator.xaml.cs
@@ -33,35 +33,39 @@ namespace AAARent
         {
             this.InitializeComponent();
             userTableobj = App.MobileService.GetTable<Radnik>();
-
+            azurirajListu();
         }
 
-        private void button_Tapped(object sender, TappedRoutedEventArgs e)
+        private async void button_Tapped(object sender, TappedRoutedEventArgs e)
         {
-            try
+            if (validacijaUnos())
             {
-                Radnik noviRadnik = new Radnik();
-                noviRadnik.Ime = textBoxIme.Text;
-                noviRadnik.Prezime = textBoxPrezime.Text;
-                noviRadnik.MaticniBroj = textBoxJMBG.Text;
-                noviRadnik.MjestoRodjenja = textBoxMjesto.Text;
-                long t = datePickerrodjen.Date.Ticks;
-                noviRadnik.DatumRodjenja = new DateTime(t);
-                t = datePickerzaposlen.Date.Ticks;
-                noviRadnik.DatumZaposlenja = new DateTime(t);
-                noviRadnik.KorisnickoIme = textBoxUsername.Text;
-                noviRadnik.Lozinka = textBoxPassword.Text;
-                noviRadnik.LjekarskiPregled = Convert.ToBoolean(checkBox.IsChecked);
-                noviRadnik.Administrator = Convert.ToBoolean(checkBox2.IsChecked);
-                noviRadnik.Plata = (float)Convert.ToDecimal(textBoxPlata.Text);
-                userTableobj.InsertAsync(noviRadnik);
-                MessageDialog msg = new MessageDialog("Unijeti novi radnik");
-                msg.ShowAsync();
-            }
-            catch(Exception ex)
-            {
-                MessageDialog msg = new MessageDialog("Error: " + ex.ToString());
-                msg.ShowAsync();
+                try
+                {
+                    Radnik noviRadnik = new Radnik();
+                    noviRadnik.Ime = textBoxIme.Text;
+                    noviRadnik.Prezime = textBoxPrezime.Text;
+                    noviRadnik.MaticniBroj = textBoxJMBG.Text;
+                    noviRadnik.MjestoRodjenja = textBoxMjesto.Text;
+                    long t = datePickerrodjen.Date.Ticks;
+                    noviRadnik.DatumRodjenja = new DateTime(t);
+                    t = datePickerzaposlen.Date.Ticks;
+                    noviRadnik.DatumZaposlenja = new DateTime(t);
+                    noviRadnik.KorisnickoIme = textBoxUsername.Text;
+                    noviRadnik.Lozinka = textBoxPassword.Password;
+                    noviRadnik.LjekarskiPregled = Convert.ToBoolean(checkBox.IsChecked);
+                    noviRadnik.Administrator = Convert.ToBoolean(checkBox2.IsChecked);
+                    noviRadnik.Plata = (float)Convert.ToDecimal(textBoxPlata.Text);
+                    await userTableobj.InsertAsync(noviRadnik);
+                    MessageDialog msg = new MessageDialog("Unijeti novi radnik");
+                    await msg.ShowAsync();
+                    azurirajListu();
+                }
+                catch (Exception ex)
+                {
+                    MessageDialog msg = new MessageDialog("Error: " + ex.ToString());
+                     await msg.ShowAsync();
+                }
             }
         }
 
@@ -69,5 +73,105 @@ namespace AAARent
         {
 
         }
+
+        private bool validacijaUnos()
+        {
+            if (textBoxIme.Text.ToString().Length == 0)
+            {
+                MessageDialog msg = new MessageDialog("Morate unijeti ime");
+                msg.ShowAsync();
+                return false;
+            }
+            if (textBoxPrezime.Text.Length == 0)
+            {
+                MessageDialog msg = new MessageDialog("Morate unijeti prezime");
+                msg.ShowAsync();
+                return false;
+            }
+            if (textBoxJMBG.Text.Length != 13)
+            {
+                MessageDialog msg = new MessageDialog("Maticni broj mora sadrzavati tacno 13 karaktera");
+                msg.ShowAsync();
+                return false;
+            }
+            if (datePickerrodjen.Date.Ticks > System.DateTime.Now.Ticks)
+            {
+                MessageDialog msg = new MessageDialog("Pogresan datum rodjenja");
+                msg.ShowAsync();
+                return false;
+            }
+            if (datePickerzaposlen.Date.Ticks > System.DateTime.Now.Ticks)
+            {
+                MessageDialog msg = new MessageDialog("Pogresan datum zaposlenja");
+                msg.ShowAsync();
+                return false;
+            }
+            if (textBoxMjesto.Text.Length == 0)
+            {
+                MessageDialog msg = new MessageDialog("Morate unijeti mjesto rodjenja");
+                msg.ShowAsync();
+                return false;
+            }
+            if (textBoxUsername.Text.Length == 0)
+            {
+                MessageDialog msg = new MessageDialog("Morate unijeti korisnicko ime");
+                msg.ShowAsync();
+                return false;
+            }
+            if (textBoxPassword.Password.Length == 0)
+            {
+                MessageDialog msg = new MessageDialog("Morate unijeti lozinku");
+                msg.ShowAsync();
+                return false;
+            }
+            if (textBoxPlata.Text.Length == 0 || Convert.ToDecimal(textBoxPlata.Text) <= 0)
+            {
+                MessageDialog msg = new MessageDialog("Plata mora biti pozitivna");
+                msg.ShowAsync();
+                return false;
+            }
+            return true;
+        }
+
+        private async void button2_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+            IMobileServiceTable<Radnik> radnici = App.MobileService.GetTable<Radnik>();
+            var rad = from x in radnici
+                      where x.MaticniBroj == textBoxJMBG2.Text && x.KorisnickoIme == textBoxUsername2.Text
+                      select x;
+            var spisak = await rad.ToListAsync();
+            if (spisak.Count == 0)
+            {
+                MessageDialog msg = new MessageDialog("Ne postoji dati radnik");
+                await msg.ShowAsync();
+            }
+            else
+            {
+                var el = spisak[0];
+                await userTableobj.DeleteAsync(el);
+                MessageDialog msg = new MessageDialog("Obrisali ste radnika");
+                await msg.ShowAsync();
+                azurirajListu();
+            }
+
+        }
+        private async void azurirajListu()
+        {
+            IMobileServiceTable<Radnik> radnici = App.MobileService.GetTable<Radnik>();
+            var rad = from x in radnici
+                      select x;
+            var spisak = await rad.ToListAsync();
+            listView.Items.Clear();
+            foreach (Radnik r in spisak)
+            {
+                listView.Items.Add(r.Ime + " " + r.Prezime + "(" + r.MaticniBroj + ", " + r.KorisnickoIme + ")");
+            }
+
+        }
+
+        
+
+        
     }
 }
