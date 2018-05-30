601de7e5928881038b4e3e7758c6e7632ac01256
Ajdin <Ajdin@DESKTOP-7662KSG>
2018-05-30 20:28:48 +0200

    klase asp

---
+++ AAARent-ASP/AAARent-ASP/Models/Klijent.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AAARent_ASP.Models
+{
+    public class Klijent
+    {
+        static int ID = 1;
+        string korisnickoIme;
+        string id;
+        string ime;
+        string prezime;
+        string brojTelefona;
+        string eMail;
+        string drzava;
+        string lozinka;
+        string maticniBroj;
+        Image slika;
+        DateTime datumRodjenja;
+        DateTime datumRegistracije;
+        string username;
+        string pw;
+
+        public Klijent()
+        {
+
+        }
+        public string MaticniBroj
+        {
+            get
+            {
+                return maticniBroj;
+            }
+            set
+            {
+                maticniBroj = value;
+            }
+        }
+        public string Lozinka
+        {
+            get
+            {
+                return lozinka;
+            }
+            set
+            {
+                lozinka = value;
+            }
+        }
+        public string KorisnickoIme
+        {
+            get
+            {
+                return korisnickoIme;
+            }
+            set
+            {
+                korisnickoIme = value;
+            }
+        }
+
+        //public Klijent(string id, string ime, string prezime, string brojTelefona, string eMail, DateTime datumRodjenja, DateTime datumRegistracije)
+
+        public Klijent(string id, string ime, string prezime, string brojTelefona, string eMail, string drzava, Image slika, DateTime datumRodjenja, DateTime datumRegistracije, string username, string pw)
+
+        {
+            this.id = id;
+            this.ime = ime;
+            this.prezime = prezime;
+            this.brojTelefona = brojTelefona;
+            this.eMail = eMail;
+            this.drzava = drzava;
+            this.slika = slika;
+            this.datumRodjenja = datumRodjenja;
+            this.datumRegistracije = datumRegistracije;
+            this.username = username;
+            this.pw = pw;
+        }
+
+        public string Drzava
+        {
+            get
+            {
+                return drzava;
+            }
+            set
+            {
+                drzava = value;
+            }
+        }
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Ime
+        {
+            get
+            {
+                return ime;
+            }
+
+            set
+            {
+                ime = value;
+            }
+        }
+
+        public string Prezime
+        {
+            get
+            {
+                return prezime;
+            }
+
+            set
+            {
+                prezime = value;
+            }
+        }
+
+        public string BrojTelefona
+        {
+            get
+            {
+                return brojTelefona;
+            }
+
+            set
+            {
+                brojTelefona = value;
+            }
+        }
+
+        public string EMail
+        {
+            get
+            {
+                return eMail;
+            }
+
+            set
+            {
+                eMail = value;
+            }
+        }
+
+        public DateTime DatumRodjenja
+        {
+            get
+            {
+                return datumRodjenja;
+            }
+
+            set
+            {
+                datumRodjenja = value;
+            }
+        }
+
+        public DateTime DatumRegistracije
+        {
+            get
+            {
+                return datumRegistracije;
+            }
+
+            set
+            {
+                datumRegistracije = value;
+            }
+        }
+
+        public System.String Username
+        {
+            get
+            {
+                return username;
+            }
+
+            set
+            {
+                username = value;
+            }
+        }
+
+        public System.String Pw
+        {
+            get
+            {
+                return pw;
+            }
+
+            set
+            {
+                pw = value;
+            }
+        }
+
+
+
+    }
+}
---
+++ AAARent-ASP/AAARent-ASP/Models/Radnik.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AAARent_ASP.Models
+{
+    public class Radnik
+    {
+
+        string id;
+        string maticniBroj;
+        string ime;
+        string prezime;
+        string mjestoRodjenja;
+        DateTime datumRodjenja;
+        DateTime datumZaposlenja;
+        string korisnickoIme;
+        string lozinka;
+        bool ljekarskiPregled;
+        bool administrator;
+        float plata;
+
+        public Radnik()
+        {
+
+        }
+
+        public float Plata
+        {
+            get
+            {
+                return plata;
+            }
+
+            set
+            {
+                plata = value;
+            }
+        }
+
+        public string ID
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public bool LjekarskiPregled
+        {
+            get
+            {
+                return ljekarskiPregled;
+            }
+
+            set
+            {
+                ljekarskiPregled = value;
+            }
+        }
+
+        public bool Administrator
+        {
+            get
+            {
+                return administrator;
+            }
+
+            set
+            {
+                administrator = value;
+            }
+        }
+
+        public string Lozinka
+        {
+            get
+            {
+                return lozinka;
+            }
+
+            set
+            {
+                lozinka = value;
+            }
+        }
+
+        public string KorisnickoIme
+        {
+            get
+            {
+                return korisnickoIme;
+            }
+
+            set
+            {
+                korisnickoIme = value;
+            }
+        }
+
+        public string MaticniBroj
+        {
+            get
+            {
+                return maticniBroj;
+            }
+
+            set
+            {
+                maticniBroj = value;
+            }
+        }
+
+        public string Ime
+        {
+            get
+            {
+                return ime;
+            }
+
+            set
+            {
+                ime = value;
+            }
+        }
+
+        public string Prezime
+        {
+            get
+            {
+                return prezime;
+            }
+
+            set
+            {
+                prezime = value;
+            }
+        }
+
+        public string MjestoRodjenja
+        {
+            get
+            {
+                return mjestoRodjenja;
+            }
+
+            set
+            {
+                mjestoRodjenja = value;
+            }
+        }
+
+        public DateTime DatumRodjenja
+        {
+            get
+            {
+                return datumRodjenja;
+            }
+
+            set
+            {
+                datumRodjenja = value;
+            }
+        }
+
+        public DateTime DatumZaposlenja
+        {
+            get
+            {
+                return datumZaposlenja;
+            }
+
+            set
+            {
+                datumZaposlenja = value;
+            }
+        }
+
+        public Radnik(string maticniBroj, string ime, string prezime, string mjestoRodjenja, DateTime datumRodjenja)
+        {
+            this.MaticniBroj = maticniBroj;
+            this.Ime = ime;
+            this.Prezime = prezime;
+            this.MjestoRodjenja = mjestoRodjenja;
+            this.DatumRodjenja = datumRodjenja;
+        }
+    }
+}
